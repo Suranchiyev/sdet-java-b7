c9df05ac3d44e6c7793b3b0d2fbaaa25337ae91c
Beknazar Suranchiyev <beknazarsuranchiyev@beknazars-mbp-2.myfiosgateway.com>
2022-09-10 18:25:05 -0400

    init commit

---
+++ src/day03/ParkingLot.java
@@ -0,0 +1,14 @@
+package day03;
+
+public class ParkingLot {
+	public static void main(String[] args) {
+		System.out.println("You said \"Java\"?");
+		System.out.println("Hmm.. it's a coffee");
+		
+		// Hello, \world\
+		System.out.println("Hello, \\world\\");
+		
+		// \ forward slash is used to escape special characters
+		// special chars: ", ', t, n, \
+	}
+}
