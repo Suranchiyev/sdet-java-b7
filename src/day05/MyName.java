43c732ce52ed6e9a8fb97e6492471a80ebd20d06
Beknazar Suranchiyev <beknazarsuranchiyev@Beknazars-MacBook-Pro-2.local>
2022-09-14 07:54:11 -0400

    day5

---
+++ src/day05/MyName.java
@@ -0,0 +1,10 @@
+package day05;
+
+public class MyName {
+	public static void main(String[] args) {
+		String firstName = "John";
+		String lastName = "Doe";
+													
+		System.out.println("Hi there, my name is " + firstName + " " + lastName + "!");
+	}
+}
---
+++ src/day05/Phone.java
@@ -0,0 +1,14 @@
+package day05;
+
+public class Phone {
+	public static void main(String[] args) {
+		String phone1, phone2;
+		phone1 = "iphone";
+		phone2 = phone1;
+		
+		System.out.println(phone1);
+		System.out.println(phone2);
+		
+		// BREAK TILL 8:26PM EST
+	}
+}
