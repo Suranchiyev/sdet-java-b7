0c4a74545dc2efa8af55d113f30ddf19ecdd055c
Beknazar Suranchiyev <beknazarsuranchiyev@beknazars-mbp-2.myfiosgateway.com>
2022-09-27 08:26:48 -0400

    day12

---
+++ src/day12/WarmUp.java
@@ -0,0 +1,30 @@
+package day12;
+
+public class WarmUp {
+	public static void main(String[] args) {
+		System.out.println(getEvenOddMsg(5)); // odd
+		System.out.println(getEvenOddMsg(2)); // even
+		System.out.println(getEvenOddMsg(4)); // even
+		System.out.println(getEvenOddMsg(10)); // ten
+	}
+	
+	/*
+	 * if argument is even return "even" 
+	 * except if argument is 10 then return "ten"
+	 * otherwise return "odd"
+	 * 
+	 * num % 2 == 0 -> even
+	 */
+	public static String getEvenOddMsg(int num) {
+		if (num == 10) {
+			// exit method and return "ten"
+			return "ten";
+		}
+		
+		if (num % 2 == 0) {
+			return "even";
+		}
+		// BREAK TILL 10:16AM EST
+		return "odd";	
+	}
+}
